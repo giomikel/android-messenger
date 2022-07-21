48a9331a6341e5b33f67c8889a88bad351a23fe4
giorgi <gmike19@freeuni.edu.ge>
2022-07-21 19:37:28 +0400

    Add homepage activity

---
+++ app/src/main/java/ge/gmikeladze/messenger/view/homepage/HomepageActivity.kt
@@ -0,0 +1,12 @@
+package ge.gmikeladze.messenger.view.homepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ge.gmikeladze.messenger.R
+
+class HomepageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
